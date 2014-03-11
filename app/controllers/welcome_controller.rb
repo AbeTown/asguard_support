d1fbe86637024b322989579f1dbe0fccb1c52e0e
AbeTown <abe.stephenson@gmail.com>
2014-03-11 01:52:44 -0700

    "Changed all of the instances of errors to bundle_errors that needed to be changed (hopefully...). Created a boring header and am having it render as part of application.html.erb."

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
 	def hello
+		@uids = Bundle.uid
+
+		flash[:alert] = "This string created with 'flash[:alert]' on welcome#hello"
+		
 	end
 
 end
