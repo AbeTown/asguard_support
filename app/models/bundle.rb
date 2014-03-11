b3c2bdaca766ef2216d528b8f82bfd7df484672e
AbeTown <abe.stephenson@gmail.com>
2014-03-10 23:54:19 -0700

    "Added two methods to the Bundle model. Not sure if they will work. Just for test"

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,2 +1,12 @@
 class Bundle < ActiveRecord::Base
+
+	# Method to return the firmware_revision and environment
+	def firmware_and_environment
+		'#{firmware_revision} + -- + #{environment}'
+	end
+
+	# Self Explanatory...
+	def time_since_bundled
+		time_elapsed = Time.now - bundled_at
+	end
 end
