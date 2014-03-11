48e3d97ee358c6b004e5d0c1994de667d6d23b7a
AbeTown <abe.stephenson@gmail.com>
2014-03-11 01:51:17 -0700

    "Added a method to determine connection type from design_subtype"

--- app/models/design.rb
+++ app/models/design.rb
@@ -2,6 +2,20 @@ class Design < ActiveRecord::Base
 
 	has_many :bundles
 
+	# The connection type as determined by the string given for design_subtype
+	def self.connection_type
+		if self.design_subtype == ('v' or 'V')
+			"virtual"
+		elsif self.design_subtype == ('g' or 'G')
+			"cellular"
+		elsif self.design_subtype == ('w' or 'W')
+			"wi-fi"
+		else
+			"wired"
+		end
+	end
+
+	# Just a more compressed version of the identification strings. 
 	def design_identifier
 		'#{design_role}' + '-' + '#{design_type}' + '#{design_subtype}'
 	end
