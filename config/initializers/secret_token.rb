0934c7cc9e2ad1d4af1784c8a0b4cd185cbdd199
AbeTown <abe.stephenson@gmail.com>
2014-03-10 22:59:14 -0700

    "Initial commit. Initialized Git. Initialized Rails. Set local version of Ruby to 1.9.3."

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsguardSupport::Application.config.secret_key_base = '27e1bcefa84b7548b00479245fa91eb596081647e61a78c59bfe1b2faee503d030048dc472716c98b8cb44fd873795a720594020ca9a557a74b84ac73c92f2b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AsguardSupport::Application.config.session_store :cookie_store, key: '_asguard_support_session'
