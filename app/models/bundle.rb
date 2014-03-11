ac1aef059bddebbcdbee77b2e6ea4fc36983cc8c
AbeTown <abe.stephenson@gmail.com>
2014-03-11 00:36:58 -0700

    "Added method to count number of bundles a company has. Added method to return the release build of a bundle, and a method to return the uid of a bundle."

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -2,6 +2,18 @@ class Bundle < ActiveRecord::Base
 
 	belongs_to :company, :design
 
+	# Scope for uid. Simple enough that this SHOULD BE DELETED LATER
+	def self.uid
+		pluck(:uid)
+	end
+
+	def self.firmware_release
+		if self.environment == ('test' or 'Test'){
+			return "Test Build"
+		}
+		else return self.firmware_revision[-3, self.firmware_revision.length]
+	end
+
 	# Method to return the firmware_revision and environment
 	def firmware_and_environment
 		'#{firmware_revision}' + '--' + '#{environment}'
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,9 @@ class Company < ActiveRecord::Base
 
 	has_many :bundles
 
+	# Return the number of bundles a company has
+	def self.number_of_bundles
+		self.count(:bundle_id)
+	end
+
 end
